101a657a134dcd5334266a19c3f7c3ae44023faa
Ian Downard <idownard@mapr.com>
2018-05-15 09:37:51 -0700

    prevent data generator from stopping

--- step1.sh
+++ step1.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Step 1 - Simulate Factory IoT data stream:
+while true; do 
 cat ~/predictive-maintenance/sample_dataset/mqtt.json | while read line; do echo $line | sed 's/{/{"timestamp":"'$(date +%s)'",/g' | /opt/mapr/kafka/kafka-*/bin/kafka-console-producer.sh --topic /apps/factory:mqtt --broker-list this.will.be.ignored:9092; sleep 1; done
+done
